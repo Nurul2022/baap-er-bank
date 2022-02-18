5e81a81089a014dd9e97e432acc428091bf9bdbe
Nurul Islam <nurulbdpersonal@gmail.com>
2022-02-18 13:31:23 +0600

    final code with updated added

--- js/bankingfinalcode.js
+++ js/bankingfinalcode.js
@@ -6,9 +6,10 @@ function doubleIt(num){
 const fiveDouble = doubleIt(5);
 const sevenDouble = doubleIt(7);*/
 
-// banking.js final code edited by me with video (27-5 to 27-9) but not exist ph1 repo. 
+// banking.js to bankingfinalcode final code edited by me with video (27-5 to 27-9) but not exist ph1 repo. 
 
 function getInputValue(inputId) {
+    debugger;
     const inputField = document.getElementById(inputId);
     const inputtAmountText = inputField.value;
     const amountValue = parseFloat(inputtAmountText);
@@ -25,11 +26,18 @@ function updateTotalField(totalFieldId, amount) {
 
     totalElement.innerText = previousTotal + amount;
 }
-
-function updateBalance(amount2, isAdd) {
+function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
+
+function updateBalance(amount2, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    // const balanceTotalText = balanceTotal.innerText;
+    // const previousBalanceTotal = parseFloat(balanceTotalText);
+    const previousBalanceTotal = getCurrentBalance();
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount2;
     }
@@ -41,13 +49,22 @@ function updateBalance(amount2, isAdd) {
 // handle deposit event handler
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
-    updateTotalField('deposit-total', depositAmount);
-    updateBalance(depositAmount, true);
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+
 });
 // handle withdraw event handler
 document.getElementById('withdraw-button').addEventListener('click', function () {
-
     const withdrawAmount = getInputValue('withdraw-input')
-    updateTotalField('withdraw-total', withdrawAmount)
-    updateBalance(withdrawAmount, false);
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount)
+        updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance) {
+        console.log('Not enough money for withdraw');
+    }
+
 })
